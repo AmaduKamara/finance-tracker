32807d7346fab4ffc10db8b160fc2074b7e5fce7
Amkam <amaduskamara@gmail.com>
2022-02-15 00:35:46 +0000

    Style Login form and set handler

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex items-center justify-center">
+      <div className="w-1/4 p-10 border rounded-lg shadow-lg bg-white mt-40">
+        <h2 className="text-3xl text-teal-600 font-thin">Login</h2>
+        <form className="mt-10" onSubmit={handleLogin}>
+          <label className="block text-left">
+            <span className="block text-lg">Email</span>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+              required
+              className="border py-4 px-3 mt-1 w-full focus:outline-teal-300"
+            />
+          </label>
+          <label className="block text-left mt-6">
+            <span className="block text-lg">Password</span>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+              required
+              className="border py-4 px-3 mt-1 w-full focus:outline-teal-300"
+            />
+          </label>
+          <div className="mt-5">
+            <button
+              type="submit"
+              className="w-full p-3 text-white bg-teal-500 rounded-md text-lg hover:text-teal-100 hover:bg-teal-700 focus:outline-teal-700"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
