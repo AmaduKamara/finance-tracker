75b01c4f76158a4e2b3af51bb155747dbe221a5f
Amkam <amaduskamara@gmail.com>
2022-04-03 06:07:11 +0000

    Conditionally show contents based on user state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 import { GiPayMoney } from "react-icons/gi";
 
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+
+  const { user } = useAuthContext();
 
   return (
     <div className="container mx-auto py-5 shadow-xl bg-white rounded-b-lg border-l-4 border-r-4 border-teal-500 flex items-center justify-between px-16">
@@ -17,20 +20,26 @@ const Navbar = () => {
         <span className="text-orange-500 text-lg">My~</span>Finance
       </Link>
       <ul className="flex items-center">
-        <li className="mx-3 text-lg hover:text-teal-600 hover:underline">
-          <Link to="/login">Login</Link>
-        </li>
-        <li className="ml-5 text-lg hover:text-teal-600 hover:underline">
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li className="ml-5 text-lg hover:text-teal-600 hover:underline">
-          <button
-            className="border py-1 px-3 rounded border-teal-500 hover:bg-teal-500 hover:text-white focus:bg-teal-600"
-            onClick={logout}
-          >
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li className="mx-3 text-lg hover:text-teal-600 hover:underline">
+              <Link to="/login">Login</Link>
+            </li>
+            <li className="ml-5 text-lg hover:text-teal-600 hover:underline">
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li className="ml-5 text-lg hover:text-teal-600 hover:underline">
+            <button
+              className="border py-1 px-3 rounded border-teal-500 hover:bg-teal-500 hover:text-white focus:bg-teal-600"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </div>
   );
