42f34e277948676951fa3bf248bc22d539fe334c
Amkam <amaduskamara@gmail.com>
2022-04-03 10:26:22 +0000

    Create handle transaction submit handler

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,16 @@ const Home = () => {
   return (
     <div className="py-16 container mx-auto flex">
       <div className="w-1/4 bg-white border shadow py-10 px-3">
-        <h1 className="text-teal-600 font-semibold text-xl md:text-2xl lg:text-3xl">
+        <h2 className="text-teal-600 font-thin text-xl md:text-2xl lg:text-3xl">
           Add Transaction
-        </h1>
+        </h2>
         <TransactionForm />
       </div>
-      <div className="w-3/4">
-        <h2>Transactions</h2>
+      <div className="w-3/4 mx-10">
+        <h1 className="text-teal-600 font-semibold text-2xl md:text-3xl lg:text-5xl">
+          Transactions
+        </h1>
+        
       </div>
     </div>
   );
--- src/pages/TransactionForm.jsx
+++ src/pages/TransactionForm.jsx
@@ -4,12 +4,21 @@ const TransactionForm = () => {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log({ name, amount });
+
+    setName("");
+    setAmount("");
+  };
+
   return (
-    <form className="border py-4 px-2 mt-5 shadow-lg">
+    <form className="border py-4 px-2 mt-5 shadow-lg" onSubmit={handleSubmit}>
       <label className="mb-4 block">
         <span className="block mb-1">Transaction name:</span>
         <input
-        className="border py-3 rounded w-full px-2 border-teal-500 focus:outline-teal-600 focus:border-2"
+          className="border py-3 rounded w-full px-2 border-teal-500 focus:outline-teal-600 focus:border-2"
           type="text"
           onChange={(e) => setName(e.target.value)}
           value={name}
@@ -20,7 +29,7 @@ const TransactionForm = () => {
       <label className="block">
         <span className="block mb-1">Transaction amount: ('$')</span>
         <input
-        className="border py-3 rounded w-full px-2 border-teal-500 focus:outline-teal-600 focus:border-2"
+          className="border py-3 rounded w-full px-2 border-teal-500 focus:outline-teal-600 focus:border-2"
           type="number"
           onChange={(e) => setAmount(e.target.value)}
           value={amount}
@@ -28,7 +37,9 @@ const TransactionForm = () => {
           required
         />
       </label>
-      <button className="py-3 w-full bg-teal-500 mt-3 rounded text-white hover:bg-teal-600 focus:bg-teal-700">Save</button>
+      <button className="py-3 w-full bg-teal-500 mt-3 rounded text-white hover:bg-teal-600 focus:bg-teal-700">
+        Save
+      </button>
     </form>
   );
 };
