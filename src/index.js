559e467a538138b2b57fb7f74d1409d0a14a6374
Amkam <amaduskamara@gmail.com>
2022-04-03 01:19:54 +0000

    Create useAuthContext hook to access the auth context

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { AuthContextProvider } from "./context/authContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 ReactDOM.render(
   <React.StrictMode>
