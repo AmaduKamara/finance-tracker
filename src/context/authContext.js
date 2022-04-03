a6bba3930314ff7f66da1b9dfc85b274a1e73550
Amkam <amaduskamara@gmail.com>
2022-04-03 06:33:29 +0000

    Add onAuthStateChanged function to maintaine logged in user status changed on login

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext();
 
@@ -8,6 +9,8 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case "LOGOUT":
       return { ...state, user: null };
+    case "AUTH_IS_READY":
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
@@ -15,10 +18,18 @@ export const authReducer = (state, action) => {
 
 const initialState = {
   user: null,
+  authIsReady: false, // hold user authentication status
 };
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
+    });
+  }, []);
 
   console.log("AuthContext state:", state);
 
